13631ea81304dc7fffbf462c49a242e285260fc0
Marcus Schimizzi <mschimiz@nd.edu>
2019-07-16 01:22:34 -0400

    changed out twitter creds

--- creds.js
+++ creds.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "consumer_key": "NvUF5w8A1ZVBXQ7S52zMv8Jfh",
-  "consumer_secret": "LjIRYw8Ci1VrFpKvXBVa5QpGOtaA8SWkoIF28w962W1MQc3QwG",
-  "access_token_key": "1014557390-rj2xh60KLsqCgL3eSWF1rXAgh7Uu5SSpbHEJlPF",
-  "access_token_secret": "gGiVpLr1DpbDlRBD3KNI7ZMqUw4jxSpIjD3zFHJ60T4Jz"
+  "consumer_key": "zmOXhX0mRKzZAubPDK3NF4xok",
+  "consumer_secret": "yH5kBzF6PIwDAk7gGXv2QTTJTcSsUVt7ggRpT6xe72AJ7APWPl",
+  "access_token_key": "1014557390-91tnlNYmJ1h0Uejv4dh6wLf7a02t55JhYQWFbuT",
+  "access_token_secret": "qUIRYPX0SbUXcbDLkmEsDEoFgxTnVjM1UuKxjVuZJZmLv"
 }
